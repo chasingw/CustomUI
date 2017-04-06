50ccd63f0fe9ee0cb688eca0a3c9a374cf84c532
wangdong <wangd101@163.com>
2017-04-06 17:39:42 +0800

    init project

---
+++ app/src/main/java/com/robosense/customui/MySeekBar.java
@@ -0,0 +1,23 @@
+package com.robosense.customui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+/**
+ * Created by maxwell on 17-4-1.
+ */
+
+public class MySeekBar extends BaseSeekBar {
+
+    public MySeekBar(Context context) {
+        super(context);
+    }
+
+    public MySeekBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MySeekBar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
---
+++ app/src/main/java/com/robosense/customui/listener/OnRangeBarChangeListener.java
@@ -0,0 +1,11 @@
+package com.robosense.customui.listener;
+
+import com.robosense.customui.BaseSeekBar;
+
+/**
+ * Created by maxwell on 17-4-1.
+ */
+
+public interface OnRangeBarChangeListener {
+    void onIndexChangeListner(BaseSeekBar rangeBar, int leftThumlIndex, int rightThumbIndex);
+}
